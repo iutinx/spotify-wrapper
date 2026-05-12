8ee4ff3e5cce9fd64cd5472ba13bd441d84beefc
iuteen <iuteendev@outlook.com>
2026-05-12 16:10:21 +0300

    init config for schemas

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,11 @@
+from app.schemas.user import UserResponse, UserProfileResponse, UserProfileRequest
+from app.schemas.auth import SpotifyLoginRequest, RefreshTokenRequest, TokenResponse
+
+__all__ = [
+    "UserResponse",
+    "UserProfileResponse",
+    "UserProfileRequest",
+    "SpotifyLoginRequest",
+    "RefreshTokenRequest",
+    "TokenResponse"
+]
